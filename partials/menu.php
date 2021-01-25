af34951b8d119058b5e88838f382386a8974b302
daniel-990 <danielarango990@gmail.com>
2021-01-25 10:32:53 -0500

    cambios en el menu

--- partials/menu.php
+++ partials/menu.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light shadow p-3 mb-5 bg-white menu-pagina">
   <div class="container-fluid">
-    <a class="navbar-brand" href="http://localhost/catalogo-dulcepina/">
+    <a class="navbar-brand" href="/">
         <img class="img-responsive icono-pagina" src="./img/pina.svg" alt="">
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
@@ -12,7 +12,7 @@
           <a class="nav-link" href="/">Inicio</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="http://localhost/catalogo-dulcepina/contacto.php">Contacto</a>
+          <a class="nav-link" href="/contacto.php">Contacto</a>
         </li>
       </ul>
     </div>
